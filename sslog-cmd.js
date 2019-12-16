268b36a32eeece1972ebcc714d5f7dc87d6a4893
Jared Krinke <jared.krinke@gmail.com>
2019-12-15 17:29:11 -0800

    Initial commit

---
+++ sslog-cmd.js
@@ -0,0 +1,22 @@
+const sslog = require("sslog");
+
+const argv = require("yargs")
+    .command("* <channel> <message>", "Log a message", yargs => 
+        yargs
+        .positional("channel", {
+            describe: "Channel to log to",
+            type: "string",
+        })
+        .positional("message", {
+            describe: "Message to log",
+            type: "string",
+        })
+        .option("importance", {
+            describe: "0 if message can be dropped",
+            type: "number",
+        })
+        .demandOption("channel")
+        .demandOption("message")
+    ).argv;
+
+sslog.log(argv.channel, argv.message, (argv.importance === 0) ? sslog.Importance.unimportant : undefined);
